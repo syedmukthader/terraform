b766072d4496da8440f081b9e0114402b348bbe2
syedmukthader <76219785+syedmukthader@users.noreply.github.com>
2022-09-15 01:56:42 +0100

    terra

--- first_instance.tf
+++ first_instance.tf
@@ -1,12 +1,11 @@
 provider "aws" {
     region = "us-east-1"
 }
-
 resource  "aws_instance" "tessst" {
    ami = "ami-05fa00d4c63e32376"
    instance_type = "t2.micro"
    availability_zone = "us-east-1a"
-   key_name = terra1
+   key_name = "terra1"
    vpc_security_group_ids = ["sg-0ec3f9ae92a8a2b11"]
    tags = {
     Name = "Terra-test"
