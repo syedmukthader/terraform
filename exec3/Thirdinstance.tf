2ffbb74855ee8cd43dca1a824527a90df2b24426
Syed-Mukthader <76219785+syedmukthader@users.noreply.github.com>
2022-09-18 04:28:37 +0100

    Add files via upload

--- exec3/Thirdinstance.tf
+++ exec3/Thirdinstance.tf
@@ -3,29 +3,28 @@ resource "aws_key_pair" "terra-key" {
   public_key = file("terraform.pub")
 }
 resource "aws_instance" "test3-teraform" {
-  ami               = var.AMIS[var.REGION]
-  instance_type     = "t2.micro"
-  availability_zone = var.ZONE1
-  key_name          = aws_key_pair.terra-key.key_name
+  ami                    = var.AMIS[var.REGION]
+  instance_type          = "t2.micro"
+  availability_zone      = var.ZONE1
+  key_name               = aws_key_pair.terra-key.key_name
   vpc_security_group_ids = ["sg-0ec3f9ae92a8a2b11"]
   tags = {
     Name    = "test3-teraform"
     Project = "Terra"
   }
- provisioner "file" {
-   source = "web.sh"
-   destination = "/tmp/web.sh"
- }
+  provisioner "file" {
+    source      = "web.sh"
+    destination = "/tmp/web.sh"
+  }
   provisioner "remote-exec" {
     inline = [
-    "chmod u+x /tmp/web.sh",
+      "chmod u+x /tmp/web.sh",
       "sudo /tmp/web.sh"
     ]
   }
-connection {
-  user = var.USER
-  private_key = file("terrakey")
-  host = self.public_ip
- }
+  connection {
+    user        = var.USER
+    private_key = file("terraform")
+    host        = self.public_ip
+  }
 }
-
