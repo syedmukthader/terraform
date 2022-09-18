6b024ce39e5c7422039e6bce729a9c1880a3fe55
Syed-Mukthader <76219785+syedmukthader@users.noreply.github.com>
2022-09-18 04:28:52 +0100

    Add files via upload

--- exec3/web.sh
+++ exec3/web.sh
@@ -2,7 +2,7 @@
 yum install wget unzip httpd -y
 systemctl start httpd
 systemctl enable httpd
-wget https://templatemo.com/tm-zip-files-2020/templatemo_510_letter.zip
-unzip -o templatemo_510_letter.zip
-cp -r templatemo_510_letter.zip/* /var/www/html
+wget https://templatemo.com/tm-zip-files-2020/templatemo_518_sentra.zip
+unzip -o templatemo_518_sentra.zip
+cp -r templatemo_518_sentra/* /var/www/html
 systemctl restart httpd
